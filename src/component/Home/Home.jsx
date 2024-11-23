0e04be162461f51d69daa178080596ec4fe8f06a
raselcsedev <raselcse.dev@gmail.com>
2024-11-23 10:29:45 +0600

    why choose scetion add

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,13 @@
+
 import SliderBanner from "../Slider/Sliderbanner";
+import WhyChoose from "../WhyChoose/WhyChoose";
+
 
 const Home = () => {
   return (
     <div className="font-bodyFont">
       <SliderBanner />
+      <WhyChoose />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import React from "react";
 
-import { Toaster } from "react-hot-toast";
-import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { store } from "./Redux/store";
 import Root from "./Root/Root";
 import Error from "./component/Error/Error";
 import Home from "./component/Home/Home";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
+import { Toaster } from "react-hot-toast";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +20,6 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
-      
     ],
   },
 ]);
