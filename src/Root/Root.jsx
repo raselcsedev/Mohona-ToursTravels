2e05b06c90881996a361925eff01853c81fb05d1
raselcsedev <raselcse.dev@gmail.com>
2024-11-23 11:36:28 +0600

    footer add

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,15 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "../component/Home/Navbar";
+import Footer from "../component/Footer/Footer";
 
 const Root = () => {
   return (
     <div className="font-bodyFont">
       <Navbar />
       <ScrollRestoration />
+
       <Outlet />
+      <Footer />
     </div>
   );
 };
