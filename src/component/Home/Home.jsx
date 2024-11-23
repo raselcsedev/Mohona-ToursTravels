98bb61d57677412729cb165ae0f0cb8847bb3818
raselcsedev <raselcse.dev@gmail.com>
2024-11-23 10:55:44 +0600

    feature section add

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,13 +1,14 @@
 
 import SliderBanner from "../Slider/Sliderbanner";
 import WhyChoose from "../WhyChoose/WhyChoose";
-
+import Feature from "../Feature/Feature";
 
 const Home = () => {
   return (
     <div className="font-bodyFont">
       <SliderBanner />
       <WhyChoose />
+      <Feature />
     </div>
   );
 };
